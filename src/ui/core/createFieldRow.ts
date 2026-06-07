790cfebbe3edc412695a058760fa0a2b83fd7071
Mathew-Pearce <matpearce1984@live.co.uk>
2026-06-08 00:55:08 +0100

    improved formatting of inspector field rows.

--- src/ui/core/createFieldRow.ts
+++ src/ui/core/createFieldRow.ts
@@ -1,13 +1,27 @@
-export function createFieldRow(
-    label,
-    value
+function formatValue(value) {
+
+  if (
+    value &&
+    typeof value === 'object' &&
+    'x' in value &&
+    'y' in value
   ) {
-  
-    const row =
-      document.createElement('div');
-  
-    row.textContent =
-      `${label}: [${value}]`;
-  
-    return row;
+    return `x: ${value.x}, y: ${value.y}`;
   }
+
+  return String(value);
+}
+
+export function createFieldRow(
+  label,
+  value
+) {
+
+  const row =
+    document.createElement('div');
+
+  row.textContent =
+    `${label}: [${formatValue(value)}]`;
+
+  return row;
+}
